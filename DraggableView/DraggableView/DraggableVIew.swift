960f4d21ac3bed890746591626142ef3834bcdb5
mantovanirian <mantovanirian@gmail.com>
2019-03-12 12:50:49 +0700

    day 3 of 100: Move Draggable view from uiviewcontroller to separate uiview

---
+++ DraggableView/DraggableView/DraggableVIew.swift
@@ -0,0 +1,86 @@
+//
+//  DraggableVIew.swift
+//  DraggableView
+//
+//  Created by Rian Putra Mantovani on 12/03/19.
+//  Copyright © 2019 Rian Putra Mantovani. All rights reserved.
+//
+
+import UIKit
+
+class DraggableVIew: UIView {
+    private var panRecognizer: UIPanGestureRecognizer!
+    private let viewModel = ViewModel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupPanGetureRecognizer()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupPanGetureRecognizer()
+    }
+    
+    func setupPanGetureRecognizer() {
+        panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragged(_:)))
+        self.addGestureRecognizer(panRecognizer)
+    }
+    
+    @objc func dragged( _ panGesture: UIPanGestureRecognizer) {
+        let translation = panGesture.translation(in: superview)
+        center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
+        panGesture.setTranslation(CGPoint.zero, in: self)
+        
+        if panGesture.state == .ended {
+            snappedtoEdge()
+        }
+    }
+    
+    private func snappedtoEdge() {
+        guard let superview = superview else {
+            assertionFailure("Draggableview has no superview")
+            return
+            
+        }
+        let coordinatePosition = viewModel.didEndDragging(Float(center.x),
+                                                          Float(center.y),
+                                                          Float(superview.center.x),
+                                                          Float(superview.center.y))
+        
+        animate(coordinatePosition)
+    }
+    
+    private func animate(_ coordinate: CoordinatePosition) {
+        UIView.animate(withDuration: 0.3, animations: { [weak self] in
+            guard let `self` = self else { return }
+            let newPoint = self.calculateNewCoordinate(coordinate)
+            self.layer.position = newPoint
+        })
+    }
+    
+    private func calculateNewCoordinate(_ coordinatePosition: CoordinatePosition) -> CGPoint {
+        guard let superview = superview else {
+            assertionFailure("Draggableview has no superview")
+            return CGPoint.zero
+        }
+        switch coordinatePosition {
+        case .top:
+            return CGPoint(x: center.x,
+                           y: frame.height / 2)
+        case .bottom:
+            return CGPoint(x: center.x,
+                           y: superview.frame.height - (frame.height / 2))
+        case .left:
+            return CGPoint(x: frame.width / 2,
+                           y: center.y)
+        case .right:
+            return CGPoint(x: superview.frame.width - (frame.width / 2),
+                           y: center.y)
+        }
+        
+    }
+    
+
+    
+}
--- DraggableView/DraggableView/ViewController.swift
+++ DraggableView/DraggableView/ViewController.swift
@@ -9,62 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let viewModel = ViewModel()
-
-    @IBAction func dragged(_ sender: UIPanGestureRecognizer) {
-        let translation = sender.translation(in: view)
-        
-        greenView.center = CGPoint(x: greenView.center.x + translation.x, y: greenView.center.y + translation.y)
-        sender.setTranslation(CGPoint.zero, in: self.view)
-
-        if sender.state == .ended {
-            snappedtoEdge()
-        }
-    }
-    
-    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var draggableView: DraggableVIew!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-    
-    private func snappedtoEdge() {
-        let coordinatePosition = viewModel.didEndDragging(Float(greenView.center.x),
-                                                          Float(greenView.center.y),
-                                                          Float(self.view.center.x),
-                                                          Float(self.view.center.y))
-        
-        animate(coordinatePosition)
-    }
-    
-    
-    private func animate(_ coordinate: CoordinatePosition) {
-        UIView.animate(withDuration: 0.3, animations: { [weak self] in
-            guard let `self` = self else { return }
-            let newPoint = self.calculateNewCoordinate(coordinate)
-            self.greenView.layer.position = newPoint
-        })
-    }
-    
-    private func calculateNewCoordinate(_ coordinatePosition: CoordinatePosition) -> CGPoint {
-        switch coordinatePosition {
-        case .top:
-            return CGPoint(x: self.greenView.center.x,
-                           y: self.greenView.frame.height / 2)
-        case .bottom:
-            return CGPoint(x: self.greenView.center.x,
-                           y: self.view.frame.height - (self.greenView.frame.height / 2))
-        case .left:
-            return CGPoint(x: self.greenView.frame.width / 2,
-                           y: self.greenView.center.y)
-        case .right:
-            return CGPoint(x: self.view.frame.width - (self.greenView.frame.width / 2),
-                           y: self.greenView.center.y)
-        }
-        
-    }
-
-
 }
 
